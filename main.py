84f4750f9d924b4b68dd7eb94b5dc2a81153d404
Moppit <michelletranusa@gmail.com>
2023-07-06 18:15:59 -0600

    tokiwa talk

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from puzzles.m_race_track import RaceTrack
 from puzzles.m_aki_talk import AkiTalk
 from puzzles.a_lunch import Lunch
 from puzzles.a_hachiken_talk import HachikenTalk
+from puzzles.e_tokiwa_talk import TokiwaTalk
 from puzzle_set import PuzzleSet
 from textwrap import dedent
 from playsound import playsound
@@ -118,13 +119,28 @@ afternoon_intro = dedent("""
               """)
 lunch = Lunch(stats, False)
 hachiken_talk = HachikenTalk(stats, False)
-afternoon = PuzzleSet(stats, [lunch,lunch,hachiken_talk])
+afternoon = PuzzleSet(stats, [lunch,lunch,hachiken_talk], introduction=afternoon_intro)
 if horse_riding_puzzle_complete and not afternoon.set_complete:
     stats = afternoon.run()
 
 # Puzzle - Baseball
+baseball_puzzle_complete = True # TODO: make actual obj
+if afternoon.set_complete and not baseball_puzzle_complete:
+    print("TODO")
 
 # Puzzle Set 3 - Evening
+evening_intro = dedent("""
+    Tokiwa: Hiya! Looks like we're assigned to work together for the evening shift!
+            Ready for some winner winner chicken pre-dinner? And by that I mean, we (both 
+            key players on the WINNING baseball team, haha!) have been assigned to the 
+            chickens for pre-dinner chores. 
+            ...
+            (We also have to dig up manure, but I'm going to ignore that for now.)
+              """)
+tokiwa_talk = TokiwaTalk(stats, False)
+evening = PuzzleSet(stats, [tokiwa_talk], introduction=evening_intro)
+if baseball_puzzle_complete and not evening.set_complete:
+    stats = evening.run()
 
 # Talk with Komaba
 
